6ffca707a966faa66572e8f1e80418dd45bfb382
nmiklavcic <nal.miklavcic@gmail.com>
2026-04-23 16:20:29 +0200

    Begining stages of tokenize implementation

--- myShell.c
+++ myShell.c
@@ -4,12 +4,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 
-int MAX_TOKENS = 1000;
+int MAX_CHARS = 1000;
 
-void tokenize()
-{
-    
+int tokenize(char * buff)
+{   
+    printf("Input line: '%s'\n", buff);
+    fflush(stdout);
+    int i = 0;
+    while( buff[i] != '\0' )
+    {
+        // we replace spaces with \0 to make suibstrings
+        if( buff[i] == ' ' )
+        {
+            buff[i] = '\0';
+            i++;
+            continue;
+        }
+        else
+        {
+            i++;
+            continue;
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char * argv[]) 
@@ -18,14 +37,26 @@ int main(int argc, char * argv[])
     while (1)
     {
         // read from stdin
-        char * buff = malloc( sizeof(char) * MAX_TOKENS );
-        int read_status = read( 0, buff, MAX_TOKENS + 1 );
-        if ( read_status == -1 )
+        char * buff = malloc( sizeof(char) * MAX_CHARS );
+        int read_status = read( 0, buff, MAX_CHARS );
+        // do stuff only if somethiung actually read :)
+        if ( 0 != (strcmp(buff, "")) )
         {
-            perror("Read: ");
-            return errno;
+            buff[read_status - 1] = '\0';
+
+            if ( read_status == -1 )
+            {
+                perror("Read: ");
+                return errno;
+            }    
+
+            tokenize(buff);
         }
+        
+        /* DEBU 
         printf("%s",buff);
+        */
+       
     }
 
 
