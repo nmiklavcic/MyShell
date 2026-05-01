a18eaa32bd9d056723270f4ad2359813e148e746
nmiklavcic <nal.miklavcic@gmail.com>
2026-05-01 16:45:38 +0200

    Changed logic for token recognition so that token printing is just that.

--- myShell.c
+++ myShell.c
@@ -11,16 +11,37 @@ int MAX_CHARS = 1000;
 int tokenize(char * buff)
 {   
     printf("Input line: '%s'\n", buff);
-    fflush(stdout);
+    fflush(stdout); 
+
+    int token_num = 0;
     int i = 0;
     int buff_size = strlen(buff);
+    int is_legit = 0;
+
     while( buff[i] != '\0' )
     {
         // we replace spaces with \0 to make suibstrings
-        if( buff[i] == ' ' )
+        if ( buff[i] == '#')
+        {
+            buff[i] == '\0';
+            if( buff[i-1] == '\0' ) token_num--;
+            break;
+        }
+        else if( buff[i] == ' ' )
         {
             buff[i] = '\0';
+            if ( i == 0 )
+            {
+                i++;
+                continue;
+            }
+            else if ( buff[i-1] == '\0' && i != 0 ) 
+            {
+                i++;
+                continue;
+            }
             i++;
+            token_num++;
             continue;
         }
         else if ( buff[i] == '"' )
@@ -28,7 +49,7 @@ int tokenize(char * buff)
             // Find next " and don't do anything with the spaces in between the two
             // DEBUG
             // printf("Currently inside of sttring recognition\n");
-            fflush(stdout);
+            // fflush(stdout);
             buff[i] = '\0';
             i++;
             while ( buff[i] != '"' )
@@ -40,39 +61,59 @@ int tokenize(char * buff)
                 if ( i == buff_size - 2 ) 
                 {
                     printf("Error! - reached end of line without second %c.\nFix input line %c should always come in pairs.",'"', '"');
+                    fflush(stdout);
+                    return 1;
                 }
                 i++;
             }
             buff[i] = '\0';
             i++;
+            token_num++;
             continue;
         }
         else
         {
+            is_legit = 1;
             i++;
             continue;
         }
     }
+    token_num++;
+    buff_size = strlen(buff);
 
-    int token_num = 0;
-
-    for ( int k = 0; k < buff_size; k += (strlen(&buff[k]) + 1 ) )
+    if(buff_size == 0 && !is_legit)
     {
-        if ( buff[k] == '\0' ) continue;
-        if( buff[k] == '#') break;
-        
-        printf("Token %d: '%s'\n", token_num, &buff[k]);
-        fflush(stdout);
-        token_num++;
+        token_num = 0;
     }
 
+    return token_num;
+}
+
+int print_tokens(char * buff, int token_num)
+{
     
+
+    int buff_size = strlen(buff);
+    int k = 0;
+
+    for ( int curr_token = 0; curr_token < token_num; curr_token++ )
+    {
+        if ( buff[k] == '\0' ) continue;
+        
+        printf("Token %d: '%s'\n", curr_token, &buff[k]);
+        fflush(stdout);
+        k += (strlen(&buff[k]) + 1 );
+    }
+
     return 0;
 }
 
 int main(int argc, char * argv[]) 
 {
     // for now lets imagine the read part as a constant while loop
+    
+    int token_count = 0;
+    
     while (1)
     {
         // read from stdin
@@ -86,7 +127,13 @@ int main(int argc, char * argv[])
         // do stuff only if somethiung actually read :)
         if (  len > 0 && buff[len - 1] == '\n')
         buff[len - 1] = '\0';
-        tokenize(buff);
+        token_count = tokenize(buff);
+
+        // DEBUG
+        printf("Token count : %d\n", token_count);
+
+        print_tokens(buff, token_count);
+
         /* DEBU 
         printf("%s",buff);
         */
